a4aeb3653082e0f785e5dd1270cc611feef6b8b8
iamlanphan <pvlan123@gmail.com>
2024-09-10 01:08:57 +0800

    project set up

---
+++ src/main/java/com/lanphan/e_commerce_monolithic/ECommerceMonolithicApplication.java
@@ -0,0 +1,13 @@
+package com.lanphan.e_commerce_monolithic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceMonolithicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceMonolithicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lanphan/e_commerce_monolithic/ECommerceMonolithicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanphan.e_commerce_monolithic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceMonolithicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
